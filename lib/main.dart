ba3e427d5aff5d90b769a37a4d9525d2219da228
Ramon Ribeiro <rhpr@cesar.org.br>
2019-11-10 07:59:21 -0300

    Add tab controller
    This patch adds a default tabcontroller with two tabs, one for gateway
    configuration over bluetooth and another for gateway discovery over
    mDNS.

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,30 @@ class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'KNoT Setup App',
-        theme: ThemeData(
-          primarySwatch: Colors.green,
-        ),
-        home: Scaffold(
+      title: 'KNoT Setup App',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
           appBar: AppBar(
             title: Text('KNoT Setup'),
+            bottom: TabBar(
+              tabs: [
+                Tab(child: Text('Configure WIFI')),
+                Tab(child: Text('Gateways connected')),
+              ],
+            ),
           ),
-          body: Center(child: Text('Knot setup app')),
-        ));
+          body: TabBarView(
+            children: [
+              Text('On page configure wifi gateway'),
+              Text('On page connected gateways')
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
