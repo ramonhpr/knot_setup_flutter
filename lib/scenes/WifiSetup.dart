607b2bc0b91e97c68f4ebadcb64607799a217bcc
Ramon Ribeiro <rhpr@cesar.org.br>
2019-11-23 22:41:08 -0300

    Render form in wifi setup widget
    This patch adds a form with fields network name and password to show
    so the user can configure which network it can connect.

--- lib/scenes/WifiSetup.dart
+++ lib/scenes/WifiSetup.dart
@@ -11,9 +11,73 @@ class WifiSetup extends StatefulWidget {
 }
 
 class WifiSetupState extends State<WifiSetup> {
+  final _formKey = GlobalKey();
   final String deviceId;
+  TextEditingController _ssidController;
+  TextEditingController _pswdController;
 
   WifiSetupState({@required this.deviceId}) : super();
+
+  @override
+  void initState() {
+    super.initState();
+    _ssidController = TextEditingController();
+    _pswdController = TextEditingController();
+  }
+
+  TextFormField _renderTextField(
+      String label, TextInputType type, TextEditingController controller,
+      {ValueChanged<String> onChanged, FormFieldValidator<String> validator}) {
+    return TextFormField(
+      keyboardType: type,
+      controller: controller,
+      obscureText: type == TextInputType.visiblePassword,
+      decoration: InputDecoration(
+        labelText: label,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(25.0),
+          borderSide: BorderSide(),
+        ),
+      ),
+      validator: validator,
+      onChanged: onChanged,
+    );
+  }
+
+  String _validatePassword(String value) {
+    if (value.isEmpty) return 'Please enter password';
+    return null;
+  }
+
+  Form _renderForm() {
+    return Form(
+      key: _formKey,
+      autovalidate: true,
+      child: ListView(
+        shrinkWrap: true,
+        padding: const EdgeInsets.symmetric(horizontal: 50),
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            child: _renderTextField(
+              "Network Name",
+              TextInputType.text,
+              _ssidController,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            child: _renderTextField(
+              "Password",
+              TextInputType.visiblePassword,
+              _pswdController,
+              validator: _validatePassword,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,10 +85,12 @@ class WifiSetupState extends State<WifiSetup> {
       title: 'Wifi configure page',
       theme: ThemeData(primarySwatch: Colors.green),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Configure Gateway Wifi'),
-        ),
-      ),
+          appBar: AppBar(
+            title: Text('Configure Gateway Wifi'),
+          ),
+          body: Center(
+            child: _renderForm(),
+          )),
     );
   }
 }
